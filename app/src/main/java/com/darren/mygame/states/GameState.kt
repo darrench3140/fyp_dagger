34c3ef213a7fbc5a8f5d0679da9860d2177c3fb0
Cheng Darren <darrench3140@gmail.com>
2023-01-10 20:32:19 +0800

    Update GameState.kt
    Dev

--- app/src/main/java/com/darren/mygame/states/GameState.kt
+++ app/src/main/java/com/darren/mygame/states/GameState.kt
@@ -7,6 +7,7 @@ data class GameState(private val status: Status = Status.RESET) {
         RESET,
         RUNNING,
         SHOOTING,
+        NEXTLEVEL,
         LOSING,
         STOPPED,
     }
@@ -14,12 +15,14 @@ data class GameState(private val status: Status = Status.RESET) {
     fun isReset() = status == Status.RESET
     fun isRunning() = status == Status.RUNNING
     fun isShooting() = status == Status.SHOOTING
+    fun isNextLevel() = status == Status.NEXTLEVEL
     fun isLosing() = status == Status.LOSING
     fun isStopped() = status == Status.STOPPED
 
     fun setReset() { gameState.value = GameState(Status.RESET) }
     fun setRunning() { gameState.value = GameState(Status.RUNNING) }
     fun setShooting() { gameState.value = GameState(Status.SHOOTING) }
+    fun setNextLevel() { gameState.value = GameState(Status.NEXTLEVEL }
     fun setLosing() { gameState.value = GameState(Status.LOSING) }
     fun setStopped() { gameState.value = GameState(Status.STOPPED) }
 }
