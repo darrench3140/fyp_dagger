8cd23a85ebd49819b5c526778306d98155a81209
darrench3140 <darrench3140@gmail.com>
2023-01-30 16:26:58 +0800

    dev

--- app/src/main/java/com/darren/fyp_dagger/screens/SettingScreen.kt
+++ app/src/main/java/com/darren/fyp_dagger/screens/SettingScreen.kt
@@ -1,6 +1,7 @@
 package com.darren.fyp_dagger.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,52 +11,73 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.darren.fyp_dagger.R
 import com.darren.fyp_dagger.utils.*
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Settings(navController: NavController, gameData: GameData) {
     DrawBackground()
-    val sections = listOf("Camera", "Facial Detection")
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(5.dp),
-        contentPadding = PaddingValues(2.dp, 50.dp, 2.dp, 70.dp)
-    ) {
-        sections.forEach { section ->
-            stickyHeader {
-                Box(modifier = Modifier.fillMaxWidth()) {
-                    Text(
-                        text = section,
-                        fontSize = 35.sp,
-                        color = yellow,
-                        fontFamily = myFont,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
-                }
-            }
-            val sectionItems = SettingsUtil.getSectionItems(section)
-            items(items = sectionItems) { settingsItem ->
-                Box(modifier = Modifier.size(screenWidthDp, 200.dp)) {
-                    Box(modifier = Modifier.size(screenWidthDp, 200.dp).background(color = Color(0x7FFFB26B)))
-                    Text(
-                        text = settingsItem.title,
-                        fontSize = 25.sp,
-                        color = white,
-                        fontFamily = myFont,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
+    Box(modifier = Modifier.fillMaxSize()) {
+        DrawShopLights()
+        Image(
+            painter = painterResource(id = R.drawable.settings3d),
+            contentDescription = "Settings 3D icon",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(y = -screenHeightDp.times(0.3f))
+                .size(screenWidthDp.div(4.935f))
+        )
+        val sections = listOf("Camera", "Facial Detection")
+        Box(modifier = Modifier
+            .align(Alignment.Center)
+            .offset(y = screenHeightDp.times(0.13f))
+            .size(screenWidthDp, screenHeightDp.times(0.6f))
+            ) {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                contentPadding = PaddingValues(2.dp, 2.dp, 2.dp, 2.dp)
+            ) {
+                sections.forEach { section ->
+                    stickyHeader {
+                        Box(modifier = Modifier
+                            .size(screenWidthDp, 70.dp)
+                            .background(Color(0xFF181D31))) {
+                            Text(
+                                text = section,
+                                fontSize = 45.sp,
+                                color = yellow,
+                                fontFamily = myFont,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    }
+                    val sectionItems = SettingsUtil.getSectionItems(section)
+                    items(items = sectionItems) { settingsItem ->
+                        Box(modifier = Modifier
+                            .size(screenWidthDp, 100.dp)
+                            .background(color = Color(0x4F181D31))) {
+                            Text(
+                                text = settingsItem.title,
+                                fontSize = 30.sp,
+                                color = white,
+                                fontFamily = myFont,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.align(Alignment.CenterStart).padding(30.dp, 0.dp, 0.dp, 0.dp)
+                            )
+
+                        }
+                    }
                 }
             }
         }
     }
-
-
     DrawReturnButton(offsetY = (-50).dp) {
         navController.popBackStack()
     }
@@ -79,6 +101,10 @@ object SettingsUtil {
         when (section) {
             "Camera" -> {
                 return listOf(
+                    SettingsItem(
+                        title = "Show Camera",
+                        type = Type.Switch,
+                    ),
                     SettingsItem(
                         title = "Rotation",
                         type = Type.Slider,
